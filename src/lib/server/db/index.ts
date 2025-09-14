ccaebf67970514d37ba93d7ccfcfb221c4613b3d
K1DV5 <kidusadugna@gmail.com>
2025-09-14 09:49:13 +0200

    make using vercel storage conditional for local

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,33 +1,68 @@
 import type { Task } from "$lib/types";
 import { put, del, list } from "@vercel/blob";
 import { BLOB_READ_WRITE_TOKEN } from '$env/static/private'
+import { readFile, rm, writeFile } from "fs/promises";
 
 const photosDir = 'photos'
 const dataPath = 'data.json'
+
+async function writeF(name: string, contents: string | File) {
+    if (!BLOB_READ_WRITE_TOKEN) {
+        await writeFile(`static/${name}`, typeof contents === 'string' ? contents : await contents.bytes())
+        return `/${name}`
+    }
+    const blob = await put(name, contents, {access: "public", token: BLOB_READ_WRITE_TOKEN, allowOverwrite: true})
+    return blob.downloadUrl
+}
+
+async function deleteF(name: string) {
+    if (!BLOB_READ_WRITE_TOKEN) {
+        await rm(name)
+        return
+    }
+    await del(name, {token: BLOB_READ_WRITE_TOKEN})
+}
+
+async function readFtxt(name: string) {
+    if (!BLOB_READ_WRITE_TOKEN) {
+        try {
+            return (await readFile(name)).toString()
+        } catch (err) {
+            if ((err as any).code !== 'ENOENT') {
+                throw err
+            }
+        }
+        return
+    }
+    const files = await list({token: BLOB_READ_WRITE_TOKEN})
+    const blob = files.blobs.find(b => b.pathname === name)
+    if (!blob) {
+        return
+    }
+    const res = await fetch(blob.downloadUrl)
+    return await res.text()
+}
 
 class Data {
     data: Task[] = []
 
     init = async () => {
-        const files = await list({token: BLOB_READ_WRITE_TOKEN})
-        const blob = files.blobs.find(b => b.pathname === dataPath)
-        if (!blob) {
+        const contents = await readFtxt(dataPath)
+        if (!contents) {
             return
         }
-        const res = await fetch(blob.downloadUrl)
-        this.data = await res.json()
+        this.data = JSON.parse(contents)
     }
 
     get = (id: string) => data.data.find(t => t.id === id)
 
     write = async () => {
-        await put(dataPath, JSON.stringify(this.data), {access: "public", token: BLOB_READ_WRITE_TOKEN, allowOverwrite: true})
+        await writeF(dataPath, JSON.stringify(this.data))
     }
 
     writePhoto = async (task: Task, photo: File) => {
         const ext = photo.name.split('.').at(-1)
-        const blob = await put(`${photosDir}/${task.id}.${ext}`, photo, {access: 'public', token: BLOB_READ_WRITE_TOKEN, allowOverwrite: true})
-        task.photo = blob.downloadUrl
+        task.photo = await writeF(`${photosDir}/${task.id}.${ext}`, photo)
     }
 
     add = async (task: Task, photo?: File) => {
@@ -41,7 +76,7 @@ class Data {
     delete = async (id: string) => {
         const task = this.data.find(t => t.id === id)
         if (task?.photo) {
-            await del(task.photo, {token: BLOB_READ_WRITE_TOKEN})
+            await deleteF(task.photo)
         }
         this.data = this.data.filter(t => t.id !== id)
         await this.write()
@@ -51,7 +86,7 @@ class Data {
         const existing = this.get(task.id)
         if (photo) {
             if (existing?.photo && existing?.photo !== photo?.name) {
-                await del(existing.photo, {token: BLOB_READ_WRITE_TOKEN})
+                await deleteF(existing.photo)
             }
             if (photo.size) {
                 await this.writePhoto(task, photo)
