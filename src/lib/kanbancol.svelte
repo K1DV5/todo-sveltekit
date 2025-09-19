fb4558f3765c4d84228d9178dc5e947d2d34a0fa
K1DV5 <kidusadugna@gmail.com>
2025-09-19 16:23:37 +0200

    add crossfade animation for kanban view

--- src/lib/kanbancol.svelte
+++ src/lib/kanbancol.svelte
@@ -4,24 +4,36 @@
     import type { Task } from "./types.js";
     import TaskItem from "./task-item.svelte";
     import Nearest from "./nearest.svelte";
+    import { receiveKanban, sendKanban } from "./animate";
+    import { flip } from "svelte/animate"
 
     const tasks = $derived(
-        sortFilterTasks(state.tasks, state.sort, state.filter),
+        sortFilterTasks(state.tasks, state.sort, state.filter)
+            .filter(([task]) => check(task)),
     );
 
-    const { title, check }: { title: string; check: (task: Task) => boolean } =
-        $props();
+    type Props = {
+        title: string
+        check: (task: Task) => boolean
+    }
+
+    const { title, check }: Props = $props();
+
 </script>
 
 <div class="p-4 m-3 bg-gray-400/5 rounded-lg flex-grow basis-40">
     <div>{title}</div>
-    {#each tasks as [task, i]}
-        {#if check(task as Task)}
+    {#each tasks as [task, i] (task.id)}
+        <div
+            in:receiveKanban={{key: task.id}}
+            out:sendKanban={{key: task.id}}
+            animate:flip={{duration: 300}}
+        >
             {#if task.id === state.nearest?.id}
                 <Nearest bind:task={state.tasks[i]} />
             {:else}
                 <TaskItem bind:task={state.tasks[i]} />
             {/if}
-        {/if}
+        </div>
     {/each}
 </div>
