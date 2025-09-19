4e12c95428504e451f9780ba07f9a30ea314a7b6
K1DV5 <kidusadugna@gmail.com>
2025-09-19 17:05:04 +0200

    add animation in transition between views

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import type { PageProps } from "./$types";
     import List from "./list.svelte";
     import { state } from "$lib/state.svelte.js";
+    import { fade } from "svelte/transition";
 
     const { data }: PageProps = $props();
 
@@ -24,15 +25,23 @@
 <div class="h-full flex flex-col">
     <Controls />
 
-    <div class="flex-grow overflow-auto">
-        {#if state.listView}
-            <List />
-        {:else}
-            {#await import('./kanban.svelte')}
-                Loading Kanban...
-            {:then {default: Kanban}} 
-                <Kanban />
-            {/await}
-        {/if}
+    <div class="flex-grow relative">
+        {#key state.listView}
+            <div
+                class="absolute inset-0 overflow-auto"
+                in:fade={{duration: 300, delay: 150}}
+                out:fade={{duration: 150}}
+            >
+                {#if state.listView}
+                    <List />
+                {:else}
+                    {#await import('./kanban.svelte')}
+                        Loading Kanban...
+                        {:then {default: Kanban}} 
+                        <Kanban />
+                    {/await}
+                {/if}
+            </div>
+        {/key}
     </div>
 </div>
