14b626fb0e24b4cd815133f23c57c629e731380c
K1DV5 <kidusadugna@gmail.com>
2025-09-20 12:00:11 +0200

    small styling fix

--- src/lib/nearest.svelte
+++ src/lib/nearest.svelte
@@ -8,7 +8,7 @@
 <div class="border rounded-xl bg-yellow-500/20 my-4 relative">
     {#if task.photo}
         <a href="/{locale}/task/{task.id}" class="block">
-            <img alt="Task" class="rounded-t w-full object-cover h-30" src={task.photo} />
+            <img alt="Task" class="rounded-t-xl w-full object-cover h-30" src={task.photo} />
         </a>
     {/if}
     <div class="p-2">
